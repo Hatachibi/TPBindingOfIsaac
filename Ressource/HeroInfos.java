3a445752bc8a59ba92c1b732c9f71c6fa92ac644
Hatachibi <67390872+Hatachibi@users.noreply.github.com>
2021-12-30 16:41:18 +0100

    La base maven

--- Ressource/HeroInfos.java
+++
@@ -1,9 +0,0 @@
-package Ressource;
-
-import Shaders.Vector2;
-
-public class HeroInfos
-{
-	public static Vector2 ISAAC_SIZE = RoomInfos.TILE_SIZE.scalarMultiplication(0.7);
-	public static final double ISAAC_SPEED = 0.01;
-}
